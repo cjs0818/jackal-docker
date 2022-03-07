18603ed98b197a08cbacd3f33779ac68a7583e41
jschoi <cjs0818@ust.ac.kr>
2022-03-08 01:13:49 +0900

    jackal_description/urdf/configs/roas added for easy switch 3D lidar and 2D lidar

--- Shared_folder/run01_jackal_L8_4_gazebo.sh
+++ Shared_folder/run01_jackal_L8_4_gazebo.sh
@@ -18,10 +18,16 @@ export CARTO_DIR="/opt/cartographer_ros/share/cartographer_ros"
 sudo cp -R $SF_DIR/jackal_description/* /opt/ros/$ROS_DISTRO/share/jackal_description/
 sudo cp $SF_DIR/jackal_navigation/launch/include/pointcloud_to_laserscan_rc.launch /opt/ros/$ROS_DISTRO/share/jackal_navigation/launch/include/
 
+
 #---- For gazebo world: 4h floor, L8 buildig with jackal
 #roslaunch jackal_gazebo jackal_world.launch config:=front_laser world_name:=/root/Shared_folder/worlds/L8_4_gazebo.world
 #roslaunch $SF_DIR/launch/$GZ_LAUNCH_FILE world_name:=$SF_DIR/worlds/$GZ_WORLD_FILE laser_model:=$JACKAL_LASER_3D_MODEL x_pos:=-8.5 y_pos:=7.7 z_pos:=1.0 yaw:=0
-roslaunch $SF_DIR/launch/$GZ_LAUNCH_FILE points_to_scan:=true world_name:=$SF_DIR/worlds/$GZ_WORLD_FILE x_pos:=-8.5 y_pos:=7.7 z_pos:=1.0 yaw:=0
+
+# For using os1-xx 3D lidar & roas.urdf.xacro
+roslaunch $SF_DIR/launch/$GZ_LAUNCH_FILE config:=roas points_to_scan:=true world_name:=$SF_DIR/worlds/$GZ_WORLD_FILE x_pos:=-8.5 y_pos:=7.7 z_pos:=1.0 yaw:=0
+	# For using lms1xx 2D lidar instead of os1-xx & roas.urdf.xacro
+	#roslaunch jackal_gazebo jackal_world.launch config:=front_laser world_name:=$SF_DIR/worlds/$GZ_WORLD_FILE x_pos:=-8.5 y_pos:=7.7 z_pos:=1.0 yaw:=0
+
 
 
 #---- For jackal teleop
