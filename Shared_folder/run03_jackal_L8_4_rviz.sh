d0a3e36eb4f4c7a2680ba643b4d44bbdc1ffb51b
jschoi <cjs0818@ust.ac.kr>
2022-03-07 18:47:53 +0900

    jackal_viz/rviz/navigation.rviz added

--- Shared_folder/run03_jackal_L8_4_rviz.sh
+++ Shared_folder/run03_jackal_L8_4_rviz.sh
@@ -5,7 +5,7 @@ export GZ_WORLD_FILE=L8_4_gazebo.world
 export MY_ROBOT=my_robot
 
 
-
+cp $SF_DIR/jackal_viz/rviz/navigation.rviz `rospack find jackal_viz`/rviz/
 
 #---- For viewing rviz
 #roslaunch jackal_viz view_robot.launch config:=localization
