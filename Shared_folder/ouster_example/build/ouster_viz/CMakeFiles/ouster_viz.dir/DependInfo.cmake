b047b0d3da9eea1269082025fa9b60b9aed2423b
jschoi <cjs0818@ust.ac.kr>
2022-03-08 01:21:52 +0900

    ouster_example folder from roas is added

---
+++ Shared_folder/ouster_example/build/ouster_viz/CMakeFiles/ouster_viz.dir/DependInfo.cmake
@@ -0,0 +1,37 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/root/Shared_folder/ouster_example/ouster_viz/src/camera.cpp" "/root/Shared_folder/ouster_example/build/ouster_viz/CMakeFiles/ouster_viz.dir/src/camera.cpp.o"
+  "/root/Shared_folder/ouster_example/ouster_viz/src/cloud.cpp" "/root/Shared_folder/ouster_example/build/ouster_viz/CMakeFiles/ouster_viz.dir/src/cloud.cpp.o"
+  "/root/Shared_folder/ouster_example/ouster_viz/src/image.cpp" "/root/Shared_folder/ouster_example/build/ouster_viz/CMakeFiles/ouster_viz.dir/src/image.cpp.o"
+  "/root/Shared_folder/ouster_example/ouster_viz/src/image_processing.cpp" "/root/Shared_folder/ouster_example/build/ouster_viz/CMakeFiles/ouster_viz.dir/src/image_processing.cpp.o"
+  "/root/Shared_folder/ouster_example/ouster_viz/src/lidar_scan_viz.cpp" "/root/Shared_folder/ouster_example/build/ouster_viz/CMakeFiles/ouster_viz.dir/src/lidar_scan_viz.cpp.o"
+  "/root/Shared_folder/ouster_example/ouster_viz/src/point_viz.cpp" "/root/Shared_folder/ouster_example/build/ouster_viz/CMakeFiles/ouster_viz.dir/src/point_viz.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GLFW_DLL"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../ouster_viz/include"
+  "../ouster_client/include"
+  "../ouster_client/include/optional-lite"
+  "/usr/include/jsoncpp"
+  "/usr/include/eigen3"
+  "ouster_client/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/root/Shared_folder/ouster_example/build/ouster_client/CMakeFiles/ouster_client.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Shared_folder/ouster_example/build/ouster_viz/CMakeFiles/simple_viz.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/root/Shared_folder/ouster_example/ouster_viz/src/simple_viz.cpp" "/root/Shared_folder/ouster_example/build/ouster_viz/CMakeFiles/simple_viz.dir/src/simple_viz.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GLFW_DLL"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../ouster_client/include"
+  "../ouster_client/include/optional-lite"
+  "/usr/include/jsoncpp"
+  "/usr/include/eigen3"
+  "ouster_client/include"
+  "../ouster_viz/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/root/Shared_folder/ouster_example/build/ouster_client/CMakeFiles/ouster_client.dir/DependInfo.cmake"
+  "/root/Shared_folder/ouster_example/build/ouster_viz/CMakeFiles/ouster_viz.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
